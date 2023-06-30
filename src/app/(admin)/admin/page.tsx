b1c3a144c3fdbc0c84ba10d3b3aa921ad6af0406
chiendc <chienzxzx33@gmail.com>
2023-06-30 14:19:14 +0700

    add codegen to git ignore

--- src/app/(admin)/admin/page.tsx
+++ src/app/(admin)/admin/page.tsx
@@ -1,11 +1,8 @@
 'use client';
 
 import DashboardPage from '@/components/admin-side/dashboard';
-import { GetAClassDocument } from '@/services/graphql-gen/graphql';
-import { useQuery } from '@apollo/client';
 
 const AdminHomePage = () => {
-  const {} = useQuery(GetAClassDocument);
   return <DashboardPage />;
 };
 
